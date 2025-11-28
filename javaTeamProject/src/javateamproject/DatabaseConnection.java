18e359a1157e95d5826fd1c5b04dae6b83875141
lenovo <lenovo@DESKTOP-ESEVVTS>
2025-11-28 17:58:59 +0200

    connection is ready now just access to the server

--- javaTeamProject/src/javateamproject/DatabaseConnection.java
+++ javaTeamProject/src/javateamproject/DatabaseConnection.java
@@ -1,8 +1,21 @@
 package javateamproject;
+
 import java.sql.*;
 
 public class DatabaseConnection {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/sportshoes";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
 
-    
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");  // الدرايفر الصحيح
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection Error: " + e);
+        }
+        return con;
+    }
 }
